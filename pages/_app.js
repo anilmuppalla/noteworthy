9a114b54c4730e329d5110c28eea664481cdc530
Anil Muppalla <anil@spotify.com>
2021-05-27 15:06:46 -0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Component {...pageProps} />;
+      </main>
+    </div>
+  );
 }
 
 export default MyApp;
